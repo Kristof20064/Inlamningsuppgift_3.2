648785888fc1e22c9f772a19a55f2b47615dbb3a
krbu06001 <krbu06001@E-PW05CGQY>
2024-11-09 09:52:03 +0100

    Add project files.

---
+++ Inlamningsuppgift_3.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Inlamning_3._2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? (skriv bokstaven j för ja och  bokstaven n för nej)");
+            string skola = Console.ReadLine();
+
+
+            Console.WriteLine("Hur gammal är du?");
+            int år = int.Parse(Console.ReadLine());
+
+            if (skola == "j" && år < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter andra personal just nu");
+            }
+
+        }
+    }
+}
